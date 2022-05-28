2c8f1db6f8e90c769d4eac97dd53a6e8a466524b
aquova <abrick912@gmail.com>
2022-05-28 10:58:13 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# YAMP
+Yet Another Markdown Parser
+
+A barebones Markdown to HTML parser written in Nim. Designed for my own usage, based on the Markdown and HTML features I use most often.
+
+## Usage
+`./yamp input.md output.html`
---
+++ yamp.nim
@@ -0,0 +1,123 @@
+# yamp
+# Yet Another Markdown Parser
+#
+# Supports:
+# Header 1-3
+# Paragraphs
+# Italics, bold, strikethru, underline
+# Ordered and unordered lists
+# Hyperlinks
+# Images
+# Code tags
+# Line breaks, horizontal lines
+
+import os, strscans
+
+proc usage() =
+    echo """
+Nim Markdown Parser
+Usage: ./yamp input.md output.html
+    """
+
+proc main() =
+    if paramCount() < 2:
+        usage()
+        quit(0)
+
+    var f: File
+    let input_filename = paramStr(1)
+    let output_filename = paramStr(2)
+
+    if not input_filename.fileExists():
+        echo("Unable to open ", input_filename)
+        quit(1)
+
+    let success = f.open(output_filename, FileMode.fmWrite)
+    if not success:
+        echo("Unable to open ", output_filename)
+        quit(1)
+
+    f.write("<!DOCTYPE html>\n<html>\n<head>\n<meta charset=\"utf-8\">\n</head>\n<body>\n")
+
+    var ordered_list, unordered_list = false
+    for line in lines(input_filename):
+        var
+            pre, txt, link, post: string
+            unused: int
+            modified_line = line
+
+        if line == "":
+            f.write("<br/>\n")
+            continue
+        elif line == "---":
+            f.write("<hr/>\n")
+            continue
+
+        # Headers
+        if line.scanf("###$s$+", txt):
+            f.write("<h3>", txt, "</h3>\n")
+            continue
+        elif line.scanf("##$s$+", txt):
+            f.write("<h2>", txt, "</h2>\n")
+            continue
+        elif line.scanf("#$s$+", txt):
+            f.write("<h1>", txt, "</h1>\n")
+            continue
+
+        # Bold
+        if modified_line.scanf("$***$+**$*", pre, txt, post):
+            modified_line = pre & "<b>" & txt & "</b>" & post
+
+        # Italics
+        if modified_line.scanf("$**$+*$*", pre, txt, post):
+            modified_line = pre & "<i>" & txt & "</i>" & post
+
+        # Strikethru
+        if modified_line.scanf("$*~~$+~~$*", pre, txt, post):
+            modified_line = pre & "<del>" & txt & "</del>" & post
+
+        # Underline
+        if modified_line.scanf("$*__$+__$*", pre, txt, post):
+            modified_line = pre & "<u>" & txt & "</u>" & post
+
+        # In-line code block
+        if modified_line.scanf("$*`$+`$*", pre, txt, post):
+            modified_line = pre & "<code>" & txt & "</code>" & post
+
+        # Lists
+        if line.scanf("$s-$s$+", txt):
+            if not unordered_list:
+                f.write("<ul>\n")
+                unordered_list = true
+            f.write("<li>", txt, "</li>\n")
+            continue
+        elif unordered_list:
+            f.write("</ul>\n")
+            unordered_list = false
+
+        if line.scanf("$s$i.$s$+", unused, txt):
+            if not ordered_list:
+                f.write("<ol>\n")
+                ordered_list = true
+            f.write("<li>", txt, "</li>\n")
+            continue
+        elif ordered_list:
+            f.write("</ol>\n")
+            ordered_list = false
+
+        # Images
+        if modified_line.scanf("$*![$+]($+)$*", pre, txt, link, post):
+            modified_line = pre & "<img src=\"" & link & "\">" & txt & "</img>" & post
+
+        # Hyperlinks
+        if modified_line.scanf("$*[$+]($+)$*", pre, txt, link, post):
+            modified_line = pre & "<a href=\"" & link & "\">" & txt & "</a>" & post
+
+        modified_line = "<p>" & modified_line & "</p>\n"
+        f.write(modified_line)
+
+    f.write("</body>\n</html>")
+    f.close()
+
+when isMainModule:
+    main()
